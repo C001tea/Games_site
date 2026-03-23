a42bdec9f5f0e840d41a2a2ab5ad11fb29e62700
C001tea <155453518+C001tea@users.noreply.github.com>
2026-03-24 00:33:50 +0200

    Первый commit: создал базу данных

---
+++ gamescatalog/catalog/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+class Store(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, verbose_name="Name")
+    domain = models.CharField(max_length=150, verbose_name="domain")
+
+    def __str__(self):
+        return self.name
+
+
+class Games(models.Model):
+    id = models.IntegerField(primary_key=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, verbose_name="Name")
+    released = models.DateField(null=True, blank=True, verbose_name="released")
+    rating = models.FloatField(default=0.0, verbose_name="rating")
+    image = models.URLField(max_length=500, null=True, blank=True, verbose_name="image")
+
+    stores = models.ManyToManyField(Store, blank=True, verbose_name="stores")
+
+    def __str__(self):
+        return self.name
+
+class Screenshot(models.Model):
+
+    game = models.ForeignKey(Games, on_delete=models.CASCADE, related_name='screenshots')
+    image = models.URLField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return f"Link for {self.game.name}"
+
+class Ratings(models.Model):
+    game = models.ForeignKey(Games, on_delete=models.CASCADE, related_name="detail_ratings")
+    title = models.CharField(max_length=100, verbose_name="rating")
+    count = models.IntegerField(default=0, verbose_name="count")
+    percent = models.FloatField(default=0.0, verbose_name="percent")
+
+    def __str__(self):
+        return f"{self.title} {self.count} for {self.game.name}"
