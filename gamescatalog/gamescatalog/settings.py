089ac01b27ed7d47b01d7fb08193a3f4ee8393cd
C001tea <155453518+C001tea@users.noreply.github.com>
2026-08-01 12:09:56 +0300

    fix celery

--- gamescatalog/gamescatalog/settings.py
+++ gamescatalog/gamescatalog/settings.py
@@ -146,8 +146,8 @@ PASSWORD_HASHERS = [
 ]
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 CELERY_TIMEZONE = 'UTC'
 
 CELERY_BEAT_SCHEDULE = {
