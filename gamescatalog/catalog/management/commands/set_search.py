d1422aabf2c5f23cc4ffd989a612ae991a194e8a
C001tea <155453518+C001tea@users.noreply.github.com>
2026-08-01 12:17:07 +0300

    set_search

--- gamescatalog/catalog/management/commands/set_search.py
+++ gamescatalog/catalog/management/commands/set_search.py
@@ -13,18 +13,17 @@ def test():
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        test()
         # print(client.index("games").update_sortable_attributes(['added', 'rating']))
-        #
-        # games = Game.objects.all()
-        # documents = [
-        #     {
-        #         "id": game.id,
-        #         "title": game.name,
-        #         "added": game.added,
-        #         "rating": float(game.rating) if game.rating else 0.0,
-        #         "alternative_names": game.alternative_names
-        #     }
-        #     for game in games
-        # ]
-        # client.index('games').add_documents(documents)
+
+        games = Game.objects.all()
+        documents = [
+            {
+                "id": game.id,
+                "title": game.name,
+                "added": game.added,
+                "rating": float(game.rating) if game.rating else 0.0,
+                "alternative_names": game.alternative_names
+            }
+            for game in games
+        ]
+        client.index('games').add_documents(documents)
